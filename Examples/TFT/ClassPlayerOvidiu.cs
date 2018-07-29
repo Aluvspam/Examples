e58b1f35c4a1807e34cbed5df69decee219f45f1
Ovidiu Othoze <ovidiu.othoze@yahoo.com>
2018-07-29 16:06:57 +0300

    epic fail

---
+++ Examples/TFT/ClassPlayerOvidiu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFT
+{
+    class ClassPlayerOvidiu : IPlayer, Iscore
+    {
+        public Moves FirstMove()
+        {
+            return RandomMove()
+        }
+
+        public Moves MyMove(Moves myLastMove, Moves othersLastMove)
+        {
+            return RandomMove();
+
+        }
+    }
+
+    internal interface Iscore
+    {
+    }
+}
