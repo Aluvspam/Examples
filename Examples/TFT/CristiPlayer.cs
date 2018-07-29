fd7ba9d6618304c9691658550a6bf01b00cf4656
Cristian232 <39384968+Cristian232@users.noreply.github.com>
2018-07-29 15:38:07 +0300

    added CristiPlayer

---
+++ Examples/TFT/CristiPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFT
+{
+    class CristiPlayer: IPlayer,IScore
+    {
+        public int Score {
+            get
+            {
+                return 0;
+            }
+        }
+
+        Random rnd;
+
+        public CristiPlayer()
+        {
+            rnd = new Random();
+        }
+
+        public Moves FirstMove()
+        {
+            return Random.Move();
+        }
+
+        public Moves MyMove(Moves myLastMove, Moves othersLastMove)
+        {
+            return RandomMove();
+        }
+
+        private Moves RandomMove()
+        {
+            return (rnd.Next(2) == 0) ? Moves.C : Moves.D;
+        }
+
+    }
+}
